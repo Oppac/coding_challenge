165b2a3f82f77a1553e3c7801309c1003c45d5df
Oppac <oppac26@gmail.com>
2019-09-19 19:17:00 +0200

    division

--- 006_mitosis/mitosis.cpp
+++ 006_mitosis/mitosis.cpp
@@ -8,55 +8,79 @@ class Cell
   public:
     sf::RenderWindow *screen;
     sf::CircleShape cell_shape;
-    float pos_x, pos_y;
-    int c_red, c_green, c_blue;
+    int radius;
+    sf::Vector2f position;
+    sf::Vector2f direction;
+    std::array<int, 3> rgb;
 
-    Cell(sf::RenderWindow *window, int radius=20)
+    Cell(sf::RenderWindow *window, int radius)
     {
       screen = window;
-      cell_shape = sf::CircleShape(radius);
+      this->radius = radius;
+
       std::random_device rd;
       std::mt19937 gen(rd());
-      pos_x = std::uniform_real_distribution<float> (0, screen->getSize().x)(gen);
-      pos_y = std::uniform_real_distribution<float> (0, screen->getSize().y)(gen);
+      position.x = std::uniform_real_distribution<float> (radius, screen->getSize().x-radius)(gen);
+      position.y = std::uniform_real_distribution<float> (radius, screen->getSize().y-radius)(gen);
+      random_direction();
 
-      c_red = std::uniform_real_distribution<float> (0, 255)(gen);
-      c_green = std::uniform_real_distribution<float> (0, 255)(gen);
-      c_red = std::uniform_real_distribution<float> (0, 255)(gen);
+      rgb[0] = std::uniform_real_distribution<float> (0, 255)(gen);
+      rgb[1] = std::uniform_real_distribution<float> (0, 255)(gen);
+      rgb[2] = std::uniform_real_distribution<float> (0, 255)(gen);
+
+      new_shape();
     }
 
-    Cell(sf::RenderWindow *window, int radius, float pos_x, float pos_y,
-         int c_red, int c_green, int c_blue)
+    Cell(sf::RenderWindow *window, int radius, sf::Vector2f position,
+         std::array<int, 3> rgb, sf::Vector2f direction)
     {
       screen = window;
-      cell_shape = sf::CircleShape(radius);
-      this->pos_x = pos_x;
-      this->pos_y = pos_y;
+      this->radius = radius;
+      this->position = position;
+      random_direction();
+      this->rgb = rgb;
 
-      this->c_red = c_red;
-      this->c_green = c_green;
-      this->c_red = c_blue;
+      new_shape();
     }
 
-    void show()
+    void new_shape()
     {
-      cell_shape.setOrigin(cell_shape.getRadius(), cell_shape.getRadius());
-      cell_shape.setPosition(pos_x, pos_y);
-
-      cell_shape.setFillColor(sf::Color(c_red, c_green, c_blue));
+      this->cell_shape = sf::CircleShape(radius);
+      cell_shape.setOrigin(radius, radius);
+      cell_shape.setFillColor(sf::Color(rgb[0], rgb[1], rgb[2]));
       cell_shape.setOutlineThickness(2);
       cell_shape.setOutlineColor(sf::Color(255, 255, 255));
-      screen->draw(cell_shape);
     }
+
+    void random_direction()
+    {
+      std::random_device rd;
+      std::mt19937 gen(rd());
+      direction.x = std::uniform_real_distribution<float> (-0.3, 0.3)(gen);
+      direction.y = std::uniform_real_distribution<float> (-0.3, 0.3)(gen);
+    }
+
+    void move()
+    {
+      position.x += direction.x;
+      position.y += direction.y;
+
+      if ((position.x > screen->getSize().x-radius) || (position.x < radius))
+        direction.x = -direction.x;
+      if ((position.y > screen->getSize().y-radius) || (position.y < radius))
+        direction.y = -direction.y;
+
+      cell_shape.setPosition(position.x, position.y);
+    }
+
 };
 
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(800, 800), "Mitosis");
 
-  int speed = 1;
   std::vector<Cell*> cells;
-  cells.push_back(new Cell(&window));
+  cells.push_back(new Cell(&window, 20));
 
   while(window.isOpen())
   {
@@ -65,19 +89,32 @@ int main()
     {
       if (event.type == sf::Event::Closed)
         window.close();
-      if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+      if (event.type == sf::Event::MouseButtonReleased)
       {
-        sf::Vector2i pixel_pos = sf::Mouse::getPosition(window);
-        sf::Vector2f mouse_pos = window.mapPixelToCoords(pixel_pos);
+        sf::Vector2i pixel_position = sf::Mouse::getPosition(window);
+        sf::Vector2f mouse_position = window.mapPixelToCoords(pixel_position);
         for (auto& cell: cells)
         {
-          if (cell->cell_shape.getGlobalBounds().contains(mouse_pos))
-          ;
+          if (cell->cell_shape.getGlobalBounds().contains(mouse_position))
+          {
+            cells.push_back(new Cell(&window, cell->radius, cell->position,
+                           cell->rgb, cell->direction));
+          }
         }
       }
     }
 
-    cells[0]->show();
+    window.clear(sf::Color::Black);
+
+    for (auto& cell: cells)
+    {
+      cell->move();
+      window.draw(cell->cell_shape);
+    }
+
     window.display();
   }
+
+  for (auto& cell: cells)
+    delete cell;
 }
